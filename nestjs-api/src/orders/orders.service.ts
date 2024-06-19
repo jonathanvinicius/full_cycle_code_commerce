01c2992a914ab12e36f4888ad474e4ce3f89a9d6
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-19 20:45:47 -0300

    feat: create orders service

--- nestjs-api/src/orders/orders.service.ts
+++ nestjs-api/src/orders/orders.service.ts
@@ -1,17 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { In, Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
 export class OrdersService {
-	create(createOrderDto: CreateOrderDto) {
-		return 'This action adds a new order';
+	constructor(
+		@InjectRepository(Order) private orderRepository: Repository<Order>,
+		@InjectRepository(Product) private readonly productRepo: Repository<Product>,
+		private amqpConnection: AmqpConnection,
+	) {}
+
+	async create(createOrderDto: CreateOrderDto & { client_id: number }) {
+		const productIds = createOrderDto.items.map((item) => item.product_id);
+
+		const uniqueProductIds = [...new Set(productIds)];
+
+		const products = await this.productRepo.findBy({
+			id: In(uniqueProductIds),
+		});
+
+		if (products.length !== uniqueProductIds.length) {
+			throw new HttpException(
+				`Algum produto não existe. Produtos passados ${productIds}, produtos encontrados ${products.map((product) => product.id)}	`,
+				HttpStatus.UNPROCESSABLE_ENTITY,
+			);
+		}
+
+		const order = Order.create({
+			client_id: createOrderDto.client_id,
+			items: createOrderDto.items.map((item) => {
+				const product = products.find((product) => product.id === item.product_id);
+				return {
+					price: product.price,
+					product_id: item.product_id,
+					quantity: item.quantity,
+				};
+			}),
+		});
+		await this.orderRepository.save(order);
+		await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+			order_id: order.id,
+			card_hash: createOrderDto.card_hash,
+			total: order.total,
+		});
+		return order;
 	}
 
-	findAll() {
-		return `This action returns all orders`;
+	findAll(client_id: number) {
+		return this.orderRepository.find({
+			where: {
+				client_id,
+			},
+			order: {
+				created_at: 'DESC',
+			},
+		});
 	}
 
-	findOne(id: number) {
-		return `This action returns a #${id} order`;
+	findOne(id: string, client_id: number) {
+		return this.orderRepository.findOneByOrFail({ id, client_id });
 	}
 }
